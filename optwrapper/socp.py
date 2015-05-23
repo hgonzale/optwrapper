adf64afc4a5a75f46c62fe288c2307879780ec52
Humberto Gonzalez <hgonzale@wustl.edu>
2015-05-23 13:57:31 -0500

    Backup

--- optwrapper/socp.py
+++ optwrapper/socp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import types
+from __future__ import division
 from optwrapper import ocp
 
 class Problem( ocp.Problem ):
@@ -326,18 +327,28 @@ class Problem( ocp.Problem ):
             return out
 
 
+        def conslinA():
+            """
+            create linear matrix for discrete input simplex constraint
+
+            """
+
+            A = np.zeros( (Nsamples, feuler.N) )
+            for k in range( Nsamples ):
+                A[ k, didx[:,k] ] = np.ones( (self.Nmodes,) )
+
+            return A
+
+
         ## setup feuler now that all the functions are defined
         feuler.initPoint( encode( self.init,
                                   np.zeros( (self.Ninputs,) ),
                                   1/self.Nmodes * np.ones( (self.Nmodes,) ) ) )
         feuler.consBox( encode( self.consstlb, self.consinlb, np.zeros( (self.Nmodes,) ) ),
                         encode( self.consstub, self.consinub, np.ones( (self.Nmodes,) ) ) )
+        feuler.consLinear( conslinA(), np.ones( (Nsamples,) ), np.ones( (Nsamples,) ) )
         feuler.objFctn( objf )
         feuler.objGrad( objg, pattern=objgpattern() )
-
-        ###############
-        ###############
-        
         feuler.consFctn( consf,
                          np.concatenate( ( np.zeros( ( dconsidx.size, ) ),
                                            np.tile( self.conslb, ( Nsamples, ) ) ) ),
@@ -347,21 +358,118 @@ class Problem( ocp.Problem ):
 
         return ( feuler, solnDecode )
 
-        # def setLinCons():
-        #     A = np.zeros( (Nsamples, N ) )
-        #     for k in range( Nsamples ):
-        #         A[ k, Nstates*(Nsamples+1) + Ninputs*Nsamples + k*Nmodes : Nstates*(Nsamples+1) + Ninputs*Nsamples + k*Nmodes + Nmodes] = 1
 
-        #     self.conslinA = A
-        #     self.conslinlb = np.ones( (Nsamples,) )
-        #     self.conslinub = np.ones( (Nsamples,) )
+    def wavelet_transform( t, arr, N ):
+        """
+        computes Haar Wavelet approximation of a nonuniform-sampled function
 
-        self.objf = objectiveFctn
-        self.objg = objectiveGrad
-        self.consf = constraintFctn
-        self.consg = constraintGrad
-        setBounds() ## this fctn sets self.lb and self.ub
-        setBoundsCons() ## this fctn sets self.conslb and self.consub
-        setLinCons()
-        self.mixedCons = False
-        self.init = np.zeros( ( self.N, ) )
+        Arguments:
+        t:   array of time samples of dimension (Nsamples+1,)
+        arr: array of function samples of dimension (Ndims,Nsamples)
+        N:   new sampling rate of the wavelet transform
+
+        Returns:
+        out: Haar wavelet transform of arr with dimension (Ndims,2**N)
+        """
+
+        t = np.array(t)
+        d = np.array(d)
+
+        if( t.size - 1 != d.size ):
+            raise ValueError("error1")
+
+        if( t[0] != 0.0 ):
+            raise ValueError("error2")
+
+        tf = 1.0
+        t0 = 0.0
+
+        deltaT = ( tf - t0 ) / (2**N)
+        t_new = np.zeros( (2**N + t.size - 1,) )
+        d_new = np.zeros( (t_new.size - 1,) )
+
+        oidx = 0
+        hidx = 1
+
+        for k in range( 0, t_new.size ):
+            # print( "t: {0}, th: {1}".format( t[oidx], deltaT*hidx ) )
+            if( oidx < d.size and t[oidx] < deltaT*hidx ):
+                t_new[k] = t[oidx]
+                d_new[k] = d[oidx]
+                oidx += 1
+            else:
+                t_new[k] = deltaT*hidx
+                if( k < d_new.size ):
+                    d_new[k] = d[oidx-1]
+                hidx += 1
+
+        # print( "t_new: {0}".format( t_new ) )
+        # print( "d_new: {0}".format( d_new ) )
+        # print( "oidx: {0}".format( oidx ) )
+
+        coeff = np.zeros( (2**N,) )
+
+        for k in range( d.size ):
+            coeff[0] += ( t[k+1] - t[k] ) * d[k]
+
+        cidx = 1
+        for k in range( N ):
+            for j in range( 2**k ):
+                low = j / 2**k
+                mid = (j + 0.5) / 2**k
+                high = (j + 1) / 2**k
+
+                idx = 0
+                while( t_new[idx] < low ):
+                    idx += 1
+
+                while( t_new[idx] < mid ):
+                    coeff[cidx] += ( t_new[idx+1] - t_new[idx] ) * d_new[idx]
+                    idx += 1
+
+                while( t_new[idx] < high ):
+                    coeff[cidx] -= ( t_new[idx+1] - t_new[idx] ) * d_new[idx]
+                    idx += 1
+
+                cidx += 1
+
+        return coeff
+
+    def inv_haar(coeff):
+        """
+        this function calculates the inverse haar transform, given the coefficients of the haar transform
+
+        input:
+        coeff: the coefficients of the haar transform; vector of size 2**N, where N is the number of Haar samples
+
+        """
+
+        coeff = np.array(coeff)
+
+        N = coeff.size
+        n = int( math.log(N, 2) )
+
+        p = np.array( [0.0, 0.0] )
+        q = np.array( [0.0, 1.0] )
+
+        for i in range(1, n):
+            p = np.append(p, i*np.ones(2**i))
+            t = np.arange(1, (2**i) + 1)
+            q = np.append(q, t)
+
+        H = np.zeros( (N, N) )
+        H[0,:] = np.ones(N)
+
+        for i in range(1,N):
+            P = p[i]
+            Q = q[i]
+            for j in range( int((N*(Q-1)/(2**P))) , int((N*((Q-0.5)/(2**P)))) ):
+                H[i,j] = 2**(P)
+            for j in range( int((N*((Q-0.5)/(2**P)))), int((N*(Q/(2**P)))) ):
+                H[i,j] = -2**(P)
+
+        H = np.transpose(H)
+
+        #print( H )
+
+        return np.dot(H, coeff)
