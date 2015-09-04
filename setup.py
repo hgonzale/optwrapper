e00500d34e57e4cb030d90475c7d07c71cdebd79
Humberto Gonzalez <hgonzale@wustl.edu>
2015-09-04 01:05:29 -0500

    preparing release of v0.5

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from Cython.Build import cythonize
 import numpy as np
 
 major = "0"
-minor = "4+"
+minor = "5"
 
 extensions = [ Extension( "optwrapper.utils",
                           sources = [ "optwrapper/utils.pyx" ],
