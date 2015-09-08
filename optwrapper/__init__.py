e296a18580b6af46424e7dcb907f7e5e6fa75954
Humberto Gonzalez <hgonzale@wustl.edu>
2015-09-07 23:53:17 -0500

    __init__.py: load all submodules automatically

--- optwrapper/__init__.py
+++ optwrapper/__init__.py
@@ -1,1 +1,8 @@
+## Import problems
+import qp, nlp, ocp, socp
+
+## Import solvers
+import snopt, npsol, lssol
+
+## Set hierarchy
 __all__ = [ "snopt", "npsol", "lssol", "qp", "nlp", "ocp", "socp" ]
